51e9111d2976329b47fbf0faf618a66e618c76c1
Velmurugan Moorthy <ext_vmoorthy@falabella.cl>
2019-06-25 17:23:18 +0530

    Vel | Java 8 streams - stream().filter() & count() method - practice code

--- Practice/src/com/practice/java8/StreamsTester.java
+++ Practice/src/com/practice/java8/StreamsTester.java
@@ -4,6 +4,7 @@
 package com.practice.java8;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -17,11 +18,22 @@ public class StreamsTester {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		StreamsTools streamTools = new StreamsTools();
+		streamTools.demoStreamOperations();
 	}
 
 }
 
+/**
+ * 
+ * @author Velmurugan
+ * Date 25-Jun-2019 | Time 3:12:53 pm
+ * 
+ * Various ways to create Streams
+ * 1. Stream.of()
+ * 2. list.stream()
+ * 3. Array.stream 
+ */
 class StreamsTools {
 	
 	public void demoStreamOperations() {
@@ -29,7 +41,17 @@ class StreamsTools {
 		List<Integer> studentIds = new ArrayList<Integer>();
 		int[] studentAges = {2893, 1, 389, 3, 2, 6,7};
 
-		//studentIds.addAll(new int[] {123,3})
+		for(int loopIndex = 1; loopIndex< 10; loopIndex++){
+			studentIds.add(loopIndex);
+        }
+		
+		//studentIds.stream().filter(predicate)
+		List<String>strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+
+		//get count of empty string
+		long count = strings.stream().filter(tempString -> !tempString.isEmpty()).count();
+		
+		System.out.println("Non Empty string count is : " + count);
 		
 	}
 }
